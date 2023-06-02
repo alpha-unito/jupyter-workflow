417b4190f81467142dc1fe5a0ad4588c2ee1bd52
GlassOfWhiskey <iacopo.c92@gmail.com>
2023-06-02 23:34:52 +0200

    Added background cell test
    This commit adds a test for `background` cells in interactive execution.
    It also solves some issues with `autoawait` cells.

--- jupyter_workflow/ipython/shell.py
+++ jupyter_workflow/ipython/shell.py
@@ -145,7 +145,7 @@ class StreamFlowInteractiveShell(ZMQInteractiveShell):
             name=cell_name, code=ast_nodes, compiler=compiler, metadata=cell_config
         )
         # Create a notebook with a single cell
-        notebook = JupyterNotebook([cell])
+        notebook = JupyterNotebook([cell], autoawait=self.autoawait)
         # Translate notebook into workflow
         translator = JupyterNotebookTranslator(context=self.context)
         workflow = await translator.translate(notebook=notebook, user_ns=self.user_ns)
@@ -312,7 +312,9 @@ class StreamFlowInteractiveShell(ZMQInteractiveShell):
                 translator = JupyterNotebookTranslator(context=self.context)
                 workflow = await translator.translate(
                     notebook=JupyterNotebook(
-                        cells=jupyter_cells, metadata=notebook.get("metadata")
+                        cells=jupyter_cells,
+                        autoawait=self.autoawait,
+                        metadata=notebook.get("metadata"),
                     ),
                     user_ns=self.user_ns,
                 )
