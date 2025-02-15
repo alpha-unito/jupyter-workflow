cfc946e2432f4a1a846a5cfc9fd2616f383a8a8e
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-02-15 22:27:23 +0100

    Update StreamFlow to version 0.2.0.dev12
    This commit updates StreamFlow to the new 0.2.0.dev12 version and
    modernizes the code.

--- jupyter_workflow/streamflow/processor.py
+++ jupyter_workflow/streamflow/processor.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
+from streamflow.core.command import CommandOutputProcessor
 from streamflow.core.deployment import Connector, Target
-from streamflow.core.workflow import CommandOutputProcessor, Job, Token, Workflow
+from streamflow.core.workflow import Job, Token, Workflow
 
 from jupyter_workflow.streamflow import utils
 from jupyter_workflow.streamflow.command import JupyterCommandOutput
