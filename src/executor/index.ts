2c561dd8d568be52e2a34c82ac2ddb60fb515d8b
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-04-10 19:28:22 +0200

    Passing notebook metadata when executing cells

---
+++ src/executor/index.ts
@@ -0,0 +1,14 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from "@jupyterlab/application";
+import { INotebookCellExecutor } from "@jupyterlab/notebook";
+import { JupyterWorkflowCellExecutor } from "./cellexecutor";
+
+export const executor: JupyterFrontEndPlugin<INotebookCellExecutor> =
+  {
+    id: 'jupyter-workflow:notebook-cell-executor',
+    description: 'Jupyter Workflow cell executor',
+    autoStart: true,
+    provides: INotebookCellExecutor,
+    activate: (app: JupyterFrontEnd): INotebookCellExecutor => {
+      return new JupyterWorkflowCellExecutor();
+    }
+  };
