005cd03176dc493b2db3735387d8cf6f07fd555a
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-04-25 16:58:18 +0200

    Uniform `cellId` impl across execution paths

--- src/icon/iconimports.ts
+++
@@ -1,9 +0,0 @@
-import { LabIcon } from "@jupyterlab/ui-components";
-
-import restartRunWorkflow from "../../style/icons/shuffle.svg";
-
-
-export const restartRunWorkflowIcon = new LabIcon({
-  name: "jupyter-workflow:restart-run-workflow ",
-  svgstr: restartRunWorkflow
-});
