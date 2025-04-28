e8444f41fb3b5d3bf2f41e88ba4afc98f595319a
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-04-28 08:11:34 +0200

    Added a first version of the editor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { JupyterFrontEndPlugin } from '@jupyterlab/application';
 import { executor } from './executor';
 import { commands } from './command';
+import { editor } from './editor';
 
-const plugins: JupyterFrontEndPlugin<any>[] = [commands, executor];
+const plugins: JupyterFrontEndPlugin<any>[] = [commands, editor, executor];
 
 export default plugins;
